aa534a3411a617e106ae218f9dee9221c7877818
Trevor Villwock <trevorvillwock@gmail.com>
2023-01-13 10:27:04 -0600

    creates grid of squares

---
+++ conways_game_of_sound.js
@@ -0,0 +1,12 @@
+//let squares = document.getElementById("grid");
+
+//squares.style.color = "blue"; 
+
+function closeModal() {
+    let modal = document.getElementById("popup");
+    /* console.log("modal:")
+    console.log(modal)
+    console.log("closing modal"); */
+    modal.style.display="none";
+    Tone.start();
+}
