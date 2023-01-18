e33ed3d2d42c7f16fc13fae1495f576ce6d838ce
Trevor Villwock <trevorvillwock@gmail.com>
2023-01-18 12:26:20 -0600

    creates grid of squares with javascript

--- conways_game_of_sound.js
+++ conways_game_of_sound.js
@@ -1,6 +1,19 @@
 //let squares = document.getElementById("grid");
 
-//squares.style.color = "blue"; 
+//squares.style.color = "blue";
+let grid;
+
+window.onload = () => {
+    grid = document.getElementById("grid");
+    console.log(grid)
+
+    for (let i = 0; i < 64; ++i) {
+        let newDiv = document.createElement("div");
+        grid.appendChild(newDiv);
+    }
+}
+
+
 
 function closeModal() {
     let modal = document.getElementById("popup");
