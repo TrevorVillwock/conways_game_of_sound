c3e4de01778e2bd262c87cb4582bcd3db9efb6dc
Trevor Villwock <trevorvillwock@gmail.com>
2023-01-26 05:21:06 -0600

    creates 10x10 grid

--- conways_game_of_sound.js
+++ conways_game_of_sound.js
@@ -3,14 +3,34 @@
 //squares.style.color = "blue";
 let grid;
 
-window.onload = () => {
-    grid = document.getElementById("grid");
-    console.log(grid)
+let rows = 10;
+let columns = 100;
 
-    for (let i = 0; i < 64; ++i) {
-        let newDiv = document.createElement("div");
-        grid.appendChild(newDiv);
+window.onload = () => {
+    // Create grid of squares
+    grid = document.getElementById("grid");
+    console.log(grid);
+
+    //let newRow = document.createElement("div");
+    //grid.appendChild(newRow);
+
+    for (let j = 0; j < columns; ++j) {
+        console.log("making rows");
+        let newColumn = document.createElement("div");
+        let divId = `r1c${j}`;
+        newColumn.id = divId;
+        grid.appendChild(newColumn);
     }
+
+    // for (let i = 0; i < rows; ++i) {
+    //     for (let j = 0; j < columns; ++j) {
+    //         let newColumn = document.createElement("div");
+    //         let divId = `r${i}c${j}`;
+    //         newColumn.id = divId;
+    //         newRow.appendChild(newColumn);
+    //     }
+    // }
+
 }
 
 
