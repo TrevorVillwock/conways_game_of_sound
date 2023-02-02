bf6693c5177242f90bb4f046c4d19e49e07e1a11
Trevor Villwock <trevorvillwock@gmail.com>
2023-02-02 15:26:57 -0600

    squares set up correctly, clock works in top left corner

--- conways_game_of_sound.js
+++ conways_game_of_sound.js
@@ -1,40 +1,12 @@
 let grid;
 let rows = 10;
 let columns = 10;
+let neighborCount = 0;
 
 // Create a 2D Array 
 let squares = Array(rows);
 for (let i = 0; i < rows; ++i) {
     squares[i] = new Array(columns);
-}
-
-window.onload = () => {
-    // Create grid of squares
-    grid = document.getElementById("grid");
-    console.log(grid);
-
-    for (let i = 0; i < rows; ++i) {
-        for (let j = 0; j < columns; ++j) {
-            let newSquare = document.createElement("div");
-            let divId = `r${i}c${j}`;
-            newSquare.id = divId;
-            newSquare.setAttribute("alive", 0);
-            newSquare.addEventListener("click", () => {
-                if (newSquare.style.backgroundColor == "white") { 
-                    newSquare.style.backgroundColor = "black";
-                    newSquare.setAttribute("alive", "0");
-                } else {
-                    newSquare.style.backgroundColor = "white";
-                    newSquare.setAttribute("alive", "1");
-                } 
-                console.log("toggling square " + divId);
-            })
-            grid.appendChild(newSquare);
-            squares[i][j] = newSquare;
-        }
-    }
-
-    // setInterval(advanceClock, 1000);
 }
 
 function closeModal() {
@@ -46,11 +18,47 @@ function closeModal() {
     Tone.start();
 }
 
+window.onload = () => {
+    // Create grid of squares
+    grid = document.getElementById("grid");
+    console.log(grid);
+
+    for (let i = 0; i < rows; ++i) {
+        for (let j = 0; j < columns; ++j) {
+            let squareHtml = document.createElement("div");
+            let divId = `r${i}c${j}`;
+            squareHtml.id = divId;
+            squareHtml.style.backgroundColor = "blue"
+            grid.appendChild(squareHtml);
+
+            let newSquare = {
+                square: squareHtml,
+                alive: 0
+            };
+
+            squareHtml.addEventListener("click", () => {
+                if (newSquare.square.style.backgroundColor == "green") { 
+                    newSquare.square.style.backgroundColor = "blue";
+                    newSquare.alive = 0;
+                } else {
+                    newSquare.square.style.backgroundColor = "green";
+                    newSquare.alive = 1;
+                } 
+                console.log("toggling square " + divId);
+            })
+            
+            squares[i][j] = newSquare;
+        }
+    }
+
+    setInterval(advanceClock, 1000);
+}
+
 function advanceClock () {
 
     for (let i = 0; i < rows; ++i) {
         for (let j = 0; j < columns; ++j) {
-            let neighborCount = 0;
+            neighborCount = 0;
 
             /* The outer if statements account for the squares along the border
             / that don't have all 8 neighbors. Categories of squares:
@@ -64,16 +72,20 @@ function advanceClock () {
             */
             
             // Top row 
-            /*
-            if (i = 0) {
+            
+            if (i == 0) {
                 // Top left corner
-                if (j = 0) {
-                    if (squares[i][j + 1].alive == "1")
+                if (j == 0) {
+                    if (squares[i][j + 1].alive == 1)
                         neighborCount++;
-                    if (squares[i+1][j].alive == "1")
+                    if (squares[i+1][j].alive == 1)
                         neighborCount++; 
-                    if (squares[i+1][j+1].alive == "1")
-                        neighborCount++;   
+                    if (squares[i+1][j+1].alive == 1)
+                        neighborCount++;  
+                    console.log(squares[i][j + 1].alive);
+                    console.log(squares[i+1][j].alive);
+                    console.log(squares[i+1][j + 1].alive);
+                    console.log("neighborCount: " + neighborCount); 
                 }
                 /*
                 else if (j = columns - 1) {
@@ -83,42 +95,41 @@ function advanceClock () {
 
                 } 
                 */
-                /*
-                console.log(neighborCount); 
             }
+            
+            
+
+            /*
+            if (squares[i-1][j] != undefined && squares[i-1][j].data-alive == "1")
+                neighborCount++;
+            if (squares[i-1][j+1] != undefined && squares[i-1][j+1].data-alive == "1")
+                neighborCount++;
+            if (squares[i+1][j] != undefined && squares[i+1][j].data-alive == "1")
+                neighborCount++;
+            if (squares[i+1][j-1] != undefined && squares[i+1][j-1].data-alive == "1")
+                neighborCount++;
+            if (squares[i+1][j+1] != undefined && squares[i+1][j+1].data-alive == "1")
+                neighborCount++;
+            if (squares[i][j-1] != undefined && squares[i][j-1].data-alive == "1")
+                neighborCount++;
+            if (squares[i-1][j-1] != undefined && squares[i-1][j-1].data-alive == "1")
+                neighborCount++;
+            if (squares[i+1][j-1] != undefined && squares[i+1][j-1].data-alive == "1")
+                neighborCount++;
             */
 
             /*
-            if (squares[i-1][j] != undefined && squares[i-1][j].alive == "1")
+            if (squares[i+1][j].data-alive == "1")
                 neighborCount++;
-            if (squares[i-1][j+1] != undefined && squares[i-1][j+1].alive == "1")
+            if (squares[i][j-1].data-alive == "1")
                 neighborCount++;
-            if (squares[i+1][j] != undefined && squares[i+1][j].alive == "1")
+            if (squares[i][j+1].data-alive == "1")
                 neighborCount++;
-            if (squares[i+1][j-1] != undefined && squares[i+1][j-1].alive == "1")
-                neighborCount++;
-            if (squares[i+1][j+1] != undefined && squares[i+1][j+1].alive == "1")
-                neighborCount++;
-            if (squares[i][j-1] != undefined && squares[i][j-1].alive == "1")
-                neighborCount++;
-            if (squares[i-1][j-1] != undefined && squares[i-1][j-1].alive == "1")
-                neighborCount++;
-            if (squares[i+1][j-1] != undefined && squares[i+1][j-1].alive == "1")
-                neighborCount++;
-            */
-
-            /*
-            if (squares[i+1][j].alive == "1")
-                neighborCount++;
-            if (squares[i][j-1].alive == "1")
-                neighborCount++;
-            if (squares[i][j+1].alive == "1")
-                neighborCount++;
-            if (squares[i-1][j-1].alive == "1")
+            if (squares[i-1][j-1].data-alive == "1")
                 neighborCount++;    
-            if (squares[i+1][j-1].alive == "1")
+            if (squares[i+1][j-1].data-alive == "1")
                 neighborCount++;
-            if (squares[i+1][j+1].alive == "1")
+            if (squares[i+1][j+1].data-alive == "1")
                 neighborCount++;
             */
 
